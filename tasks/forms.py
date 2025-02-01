8db88265ea7e1e8e4d1a78bbabc1c40eb5635574
Juwel Saha <juwelsaha14@gmail.com>
2025-02-01 16:47:29 +0600

    create task into Database Using Form Data

---
+++ tasks/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+class TaskForm(forms.Form):
+    title = forms.CharField(max_length=250, label="Task Title")
+    description = forms.CharField(
+        widget=forms.Textarea, label="Task Description")
+    due_date = forms.DateField(widget=forms.SelectDateWidget, label="Due Date")
+    assigned_to = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple, choices=[], label="Assigned To")
+
+    def __init__(self, *args, **kwargs):
+        employees = kwargs.pop("employees", [])
+        print(employees)
+        super().__init__(*args, **kwargs)
+        self.fields["assigned_to"].choices = [(emp.id, emp.name) for emp in employees]
+        
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.forms import TaskForm
+from tasks.models import Employee, Task
 
 # Create your views here.
 
@@ -26,4 +28,27 @@ def test(request):
 
 
 def create_task(request):
-    return render(request, "task_form.html")
+    employees = Employee.objects.all()
+    form = TaskForm(employees = employees)
+    if request.method == "POST":
+        form = TaskForm(request.POST, employees = employees)
+        if form.is_valid():
+            data = form.cleaned_data
+            title = data.get("title")
+            description = data.get("description")
+            due_date = data.get("due_date")
+            assigned_to = data.get("assigned_to")
+
+            task = Task.objects.create(
+                title = title,
+                description = description,
+                due_date = due_date,
+            )
+            for emp_id in assigned_to:
+                employee = Employee.objects.get(id=emp_id)
+                task.assigned_to.add(employee)
+            return HttpResponse("Task Created Successfully")
+
+            
+    context = {"form": form}
+    return render(request, "task_form.html", context)
